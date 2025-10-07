85aaec522acad66eeda3489b276a77a54281149a
nicolaiprodromov <nicolaiprodromov@users.noreply.github.com>
2025-10-07 12:20:15 +0300

    docs: add readme

--- src/compiler.py
+++ src/compiler.py
@@ -9,12 +9,10 @@ class Compiler():
     def compile(self):
         global global_vars
         addon_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        static_dir = os.path.join(addon_dir, "static")
-        
         for _script_ in self.ui.theme.scripts:
             module_name = _script_.replace(".py", "")
             try:
-                script_path = os.path.join(static_dir, f"{module_name}.py")
+                script_path = os.path.join(addon_dir, f"{module_name}.py")
                 spec = importlib.util.spec_from_file_location(module_name, script_path)
                 module = importlib.util.module_from_spec(spec)
                 spec.loader.exec_module(module)
