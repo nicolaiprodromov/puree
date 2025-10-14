f0a03fc53dadca51d59bf1193cb29364a6be303d
nicolaiprodromov <prodromounicolaie@gmail.com>
2025-10-14 13:36:30 +0300

    chore: bump version to 0.0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name                          = "puree-ui",
-    version                       = "0.0.8",
+    version                       = "0.0.9",
     author                        = "Nicolai Prodromov",
     description                   = "XWZ Puree UI framework for Blender",
     long_description              = long_description,
