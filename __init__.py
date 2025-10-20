2d799a3274cf1dcfaa3486135de7f3ef41ba8ddd
nicolaiprodromov <prodromounicolaie@gmail.com>
2025-10-20 16:23:24 +0300

    chore: bump version to 0.1.2

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from puree import set_addon_root
 bl_info = {
     "name"       : "Puree",
     "author"     : "Nicolai Prodromov",
-    "version"    : (0, 1, 1),
+    "version"    : (0, 1, 2),
     "blender"    : (4, 2, 0),
     "location"   : "3D View > Sidebar > Puree",
     "description": "XWZ Puree UI framework",
