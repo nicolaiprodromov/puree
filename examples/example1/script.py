ae57e854871d6a5d0d60b4fc0297cb48176bbb7a
nicolaiprodromov <prodromounicolaie@gmail.com>
2025-10-22 16:33:24 +0300

    docs(examples): continue examples

--- examples/example1/script.py
+++ examples/example1/script.py
@@ -1,45 +1,60 @@
-from puree.utils import recursive_search
+# Example 1 [CORE UI VOL. I]
+# Created by XWZ
+# ◕‿◕ Distributed for free at:
+# https://github.com/nicolaiprodromov/puree/wiki
+# ╔═════════════════════════════════╗
+# ║  ██   ██  ██      ██  ████████  ║
+# ║   ██ ██   ██  ██  ██       ██   ║
+# ║    ███    ██  ██  ██     ██     ║
+# ║   ██ ██   ██  ██  ██   ██       ║
+# ║  ██   ██   ████████   ████████  ║
+# ╚═════════════════════════════════╝
+
 def main(self, app):
 
-    radial_focus        = [.2, .8, .032, .165]
+    #rgba(221, 221, 221, 0.479)
+    radial_focus        = [.88, .88, .88, .25]
     radial_neutral      = [0, 0, 0, 0]
     radial_blur_focus   = 13
     radial_blur_neutral = 0
 
     def hover_button_in(container):
-        this_container = recursive_search(app.theme.root, container['id'])
+        this_container = app.get_by_id(container['id'])
         radial = this_container.children[2]
         radial.style.box_shadow_color = radial_focus
         radial.style.box_shadow_blur  = radial_blur_focus
         radial.mark_dirty() 
 
     def hover_button_out(container):
-        this_container = recursive_search(app.theme.root, container['id'])
+        this_container = app.get_by_id(container['id'])
         radial = this_container.children[2]
         radial.style.box_shadow_color = radial_neutral
         radial.style.box_shadow_blur  = radial_blur_neutral
         radial.mark_dirty()
 
     def click_button(container):
-        this_container = recursive_search(app.theme.root, container['id'])
+        this_container = app.get_by_id(container['id'])
         radial = this_container.children[2]
         radial.style.box_shadow_color = radial_focus
         radial.style.box_shadow_blur  = radial_blur_focus
         radial.mark_dirty() 
 
     def toggle_button(container):
-        this_container = recursive_search(app.theme.root, container['id'])
+        this_container = app.get_by_id(container['id'])
         radial = this_container.children[2]
-        print(f"Toggle - Container: {container['id']}, Toggle: {container['_toggle_value']}, Radial: {radial.id}, Style Obj: {id(radial.style)}")
+
+        test_move_cont = app.get_by_id("default_label")
 
         if container['_toggle_value'] == True:
             radial.style.box_shadow_color = radial_focus
             radial.style.box_shadow_blur  = radial_blur_focus
+            test_move_cont.set_property('width', '22px')
             radial.mark_dirty() 
         else:
             radial.style.box_shadow_color = radial_neutral
             radial.style.box_shadow_blur  = radial_blur_neutral
-            radial.mark_dirty() 
+            test_move_cont.set_property('width', '100%')
+            radial.mark_dirty()
 
     app.get_by_id("hover_test_button").hover.append(hover_button_in)
     app.get_by_id("hover_test_button").hoverout.append(hover_button_out)
--- examples/example2/__init__.py
+++ examples/example2/__init__.py
@@ -1,1 +1,12 @@
+# Example 2 [CORE UI VOL. I]
+# Created by XWZ
+# ◕‿◕ Distributed for free at:
+# https://github.com/nicolaiprodromov/puree/wiki
+# ╔═════════════════════════════════╗
+# ║  ██   ██  ██      ██  ████████  ║
+# ║   ██ ██   ██  ██  ██       ██   ║
+# ║    ███    ██  ██  ██     ██     ║
+# ║   ██ ██   ██  ██  ██   ██       ║
+# ║  ██   ██   ████████   ████████  ║
+# ╚═════════════════════════════════╝
 # this file can be empty, but it has to be here
