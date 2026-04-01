b03cfc05c6c6c6cd316562704b7ffa0064020e29
nicolaiprodromov <nicolaiprodromov@users.noreply.github.com>
2026-04-01 22:50:13 +0300

    chore: test console module

--- tests/helloworld/script.py
+++ tests/helloworld/script.py
@@ -11,6 +11,8 @@ _color_index = 0
 
 def main(self, app):
     button = app.theme.root.button
+    button_2 = app.theme.root.button_2
+    button_3 = app.theme.root.button_3
 
     def on_click(container):
         global _color_index
@@ -18,6 +20,15 @@ def main(self, app):
         base, hover = colors[_color_index]
         button.set_property("background-color", base)
         button.mark_dirty()
+        console.log("Color changed to", base)
+
+    def on_click_2(container):
+        console.warn("Warning from button_2")
+
+    def on_click_3(container):
+        console.error("Error from button_3")
 
     button.click.append(on_click)
+    button_2.click.append(on_click_2)
+    button_3.click.append(on_click_3)
     return app
